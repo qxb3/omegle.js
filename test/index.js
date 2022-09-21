bc856348eec5f18bb470b66c178a9c697183a32b
qxb3 <qxbthree@gmail.com>
2022-09-21 17:12:37 +0800

    Fix sendMessage()

--- test/index.js
+++ test/index.js
@@ -1,27 +1,26 @@
 import OmegleClient from '../lib/OmegleClient.js'
 
 const client = new OmegleClient()
+let interval
 
 client.on('connected', () => {
   console.log('Connected')
   client.sendMessage(`Hello! I will copy everything that you say!`)
+
+  interval = setInterval(() => {
+    console.log(client.messages)
+  }, 1000)
 })
 
 client.on('message', (message) => {
   console.log(message)
-
-  setTimeout(() => {
-    client.startTyping()
-    client.sendMessage(`[BOT] - ${message}`)
-    client.stopTyping()
-
-    console.log(client.messages)
-  }, 3000)
+  client.sendMessage(message)
 })
 
 client.on('disconnected', () => {
   console.log('Disconnected')
   client.connect({ topics: ['friend', 'friends'] })
+  clearInterval(interval)
 })
 
 client.connect({ topics: ['friend', 'friends'] })
