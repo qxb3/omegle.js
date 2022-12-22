08b1e4d9ced67cc40dcf4c562df496eee2b89044
qxb3 <qxbthree@gmail.com>
2022-12-22 10:28:43 +0800

    feat: added dts & build steps

--- tsup.config.js
+++ tsup.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  format: ['cjs', 'esm'],
-  clean: true,
-  esbuildOptions: (options) => {
-    options.footer = {
-      js: 'module.exports = module.exports.default;'
-    }
-  },
-  entry: ['lib/index.js'],
-  outDir: 'build'
+export default defineConfig((options) => {
+  return {
+    format: ['cjs', 'esm'],
+    dts: options.dts,
+    watch: options.watch,
+    clean: true,
+    esbuildOptions: (options) => {
+      options.footer = {
+        js: 'module.exports = module.exports.default;'
+      }
+    },
+    entry: ['lib/index.js'],
+    outDir: 'build'
+  }
 })
